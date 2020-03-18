32ec0d721430412dc8dfdc2bf5853778373e6e54
afeiship <1290657123@qq.com>
2020-03-18 23:48:35 +0800

    fix: revert normal verion

--- src/index.js
+++ src/index.js
@@ -16,18 +16,13 @@ export default async (inOptions) => {
   const timeout = { timeout: 0, waitUntil: 'domcontentloaded' };
   const browser = await puppeteer.launch(options);
   const page = await browser.newPage();
-  console.log('new page...!');
   await page.goto('https://pan.baidu.com/');
-  console.log('goto pan.baidu. click qq login!');
   await page.waitForSelector('.bd-acc-qzone');
   await page.click('.bd-acc-qzone .phoenix-btn-item');
-  console.log('click qq login!');
-
   return new Promise((resolve) => {
     page.addListener('popup', async (popup) => {
       await ssoQqPtlogin2(popup, inOptions);
-      console.log('qq login pass!');
-      await page.goto('https://pan.baidu.com/disk/home', timeout);
+      await page.goto('https://pan.baidu.com/disk/home', { timeout: 0, waitUntil: 'domcontentloaded' });
       const cookies = await page.cookies();
       const html = await page.content();
       const matches = html.match(TOKEN_RE);
