8fdbef68a16abfe7837b161de3e89b4a519ecaa4
afeiship <1290657123@qq.com>
2020-03-18 23:45:59 +0800

    test: version

--- src/index.js
+++ src/index.js
@@ -16,12 +16,17 @@ export default async (inOptions) => {
   const timeout = { timeout: 0, waitUntil: 'domcontentloaded' };
   const browser = await puppeteer.launch(options);
   const page = await browser.newPage();
-  await page.goto('https://pan.baidu.com/', timeout);
+  console.log('new page...!');
+  await page.goto('https://pan.baidu.com/');
+  console.log('goto pan.baidu. click qq login!');
   await page.waitForSelector('.bd-acc-qzone');
   await page.click('.bd-acc-qzone .phoenix-btn-item');
+  console.log('click qq login!');
+
   return new Promise((resolve) => {
     page.addListener('popup', async (popup) => {
       await ssoQqPtlogin2(popup, inOptions);
+      console.log('qq login pass!');
       await page.goto('https://pan.baidu.com/disk/home', timeout);
       const cookies = await page.cookies();
       const html = await page.content();
